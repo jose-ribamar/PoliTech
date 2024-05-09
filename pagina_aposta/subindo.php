e576ca0477536db8ca2a98d3cf4e8396c8c25a87
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-05-08 23:40:14 -0300

    Exclusão de arquivos desnecessários

--- pagina_aposta/subindo.php
+++ pagina_aposta/subindo.php
@@ -1,19 +1,4 @@
-<?php
-    if(isset($_POST['acao'])){
-        $arquivo = $_FILES['file'];
 
-        $arquivo_novo = explode('.', $arquivo['name'] );
-
-        if($arquivo_novo[sizeof($arquivo_novo)-1] != 'png'){
-            die('Você não pode fazer upload deste tipo de aruivo');
-
-        }else{
-            echo 'Podemos continuar...';
-            move_uploaded_file($arquivo['tmp_name'], '../uploads/'.$arquivo['name']);
-        }
-    };
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,8 +8,9 @@
 </head>
 <body>
 
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="file"/>
+    <form action="cadastro_img.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="foto"/>
+        <input type="text" name="nome_imagem" id="idnome_imagem">
         <input type="submit" name="acao" value="Enviar">
     </form>
 </body>
