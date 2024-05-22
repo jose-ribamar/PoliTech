e1778edf83ec2a1fa3b0b1c5aadad4b01a6ca07c
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-05-21 23:03:29 -0300

    Adição de um campo que deleta a imagem

--- campo_img/visualizar_img.php
+++ campo_img/visualizar_img.php
@@ -1,11 +1,20 @@
 <?php 
-
 include_once('../banco/config.php');
 
-$sql_query = $mysqli -> query("SELECT * FROM principais") or die($mysqli -> error)
+// if(isset($_GET['deletar'])){
+//     $id = intval($_GET['deletar']);
+//     $sql_query = $mysqli->query("SELECT * FROM principais WHERE id='$id'") or die($mysqli->error);
+//     $arquivo = $sql_query->fetch_assoc();
 
+//     if(unlink($arquivo['path'])){
+//         $apago_imagem = $mysqli->query("DELETE FROM principais WHERE id='$id'") or die($mysqli->error);
+//         if($apago_imagem){
+//             echo "<p>deletado com sucesso!<p>";
+//         }
+//     }
+// }
 
-
+$sql_query = $mysqli->query("SELECT * FROM principais") or die($mysqli->error);
 ?>
 
 <!DOCTYPE html>
@@ -27,23 +36,21 @@ $sql_query = $mysqli -> query("SELECT * FROM principais") or die($mysqli -> erro
         </tr>
     </thead>
 
-
     <tbody>
     <?php 
-    while($arquivo = $sql_query-> fetch_assoc()){
-
+    while($arquivo = $sql_query->fetch_assoc()){
     ?> 
         <tr>
             <td> <a target="_blank" href="<?= $arquivo['path']; ?>"><img height="50" src="<?=$arquivo['path'] ?>" alt=""></a></td>
             <td><?= $arquivo['nome_imagem']; ?> </td>
             <td><?= date("d/m/y", strtotime($arquivo['upload_date']))  ?> </td>
+            <td><a href="deletar_img.php?deletar=<?= $arquivo['id']; ?>">Deletar</a></td>
         </tr>
-
-        <?php 
+    <?php 
     }
-        ?>
+    ?>
     </tbody>
 </table>
-
+<a href="../pagina_aposta/subindo.php"?>Voltar</a>
 </body>
 </html>
